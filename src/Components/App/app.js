f20ca52d783443685ad590fa87cf577e88d619dc
Jony-K <jonykapash@gmail.com>
2020-09-29 15:58:00 +0300

    Working Add/Remove buttons + linked save to spotify button

--- src/Components/App/app.js
+++ src/Components/App/app.js
@@ -55,6 +55,8 @@ class App extends React.Component {
 		};
 		this.addTrack = this.addTrack.bind(this);
 		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlaylistName = this.updatePlaylistName.bind(this);
+		this.savePlaylist = this.savePlaylist.bind(this);
 	}
 
 	addTrack(track) {
@@ -75,6 +77,14 @@ class App extends React.Component {
 		});
 	}
 
+	updatePlaylistName(name) {
+		this.setState({ playlistName: name });
+	}
+
+	savePlaylist() {
+		const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+	}
+
 	render() {
 		return (
 			<div>
@@ -92,6 +102,8 @@ class App extends React.Component {
 							playlistName={this.state.playlistName}
 							playlistTracks={this.state.playlistTracks}
 							onRemove={this.removeTrack}
+							onNameChange={this.updatePlaylistName}
+							onSave={this.savePlaylist}
 						/>
 					</div>
 				</div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,17 +5,27 @@ import "./Playlist.css";
 // Adds a new playlist from the SearchResults to Spotify
 
 class Playlist extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleNameChange = this.handleNameChange.bind(this);
+	}
+	handleNameChange(event) {
+		this.props.onNameChange(event.target.value);
+	}
+
 	render() {
 		return (
 			<div className="Playlist">
-				<input defaultValue={"New Playlist"} />
+				<input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
 				<TrackList
 					tracks={this.props.playlistTracks}
 					onRemove={this.props.onRemove}
 					isRemoval={true}
 				/>
 
-				<button className="Playlist-save">SAVE TO SPOTIFY</button>
+				<button className="Playlist-save" onClick={this.props.onSave}>
+					SAVE TO SPOTIFY
+				</button>
 			</div>
 		);
 	}
